09f2f9b21797e3021f31f36189cfcbe61f73641d
Marco Aurelio Hansen de Oliveira <SSNPETT18@firjan.com.br>
2025-06-30 20:49:18 -0300

    contador

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Contador } from "./components/Contador/Contador";
+
+function App() {
+  return (
+    <>
+      <Contador />
+      <Contador />
+      <Contador />
+    </>
+  );
+}
+
+export default App;
